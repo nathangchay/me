da22bdad6d69bf2e4fc024b35593babb3142698a
nate <nathangchay@gmail.com>
2021-07-29 16:03:28 -0600

    Create Main.vue

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>blank</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  color: '#000',
+}
+</style>
