511544330c34cbeb7fe9df7dc83a32273746fcce
nate <nathangchay@gmail.com>
2021-07-31 14:24:59 -0600

    Create TitleCard.vue

---
+++ src/components/TitleCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="container">
+    <h1>natetheneet</h1>
+    <h3>software engineer</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleCard',
+};
+</script>
+
+<style scoped>
+ #container {
+   padding: 50px;
+   background-color: rgb(30, 30, 30);
+   box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+   border-radius: 10px;
+ }
+</style>
