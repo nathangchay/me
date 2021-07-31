afa51e9dddec0465d3f6c66a631445e2bae27fd9
nate <nathangchay@gmail.com>
2021-07-31 14:24:49 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
-  <Main />
+  <TitleCard />
 </template>
 
 <script>
-import Main from './components/Main.vue';
+import TitleCard from './components/TitleCard.vue';
 
 export default {
   name: 'App',
   components: {
-    Main,
+    TitleCard,
   },
 };
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Inconsolata&display=swap');
+
+  html, body {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    background-color: rgb(40, 40, 40);
+    display: flex;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: Inconsolata, Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #4e5b69;
-    margin-top: 60px;
+    color: #fff;
   }
 </style>
