1b813e2caa8d2fb653ece33dccee8dd4bd411045
nate <nathangchay@gmail.com>
2021-08-02 16:07:34 -0600

    Added icon buttons

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -1,7 +1,31 @@
 <template>
   <div id="container">
-    <h1>natetheneet</h1>
-    <h3>software engineer</h3>
+    <div id="icons">
+      <a href=https://github.com/natetheneet>
+        <img src="../assets/github.png" />
+      </a>
+
+      <a href=https://twitter.com/latencygirl>
+        <img src="../assets/twitter.png" />
+      </a>
+
+      <a href=https://www.instagram.com/natetheneet/>
+        <img src="../assets/instagram.png" />
+      </a>
+
+      <a href=https://www.linkedin.com/in/nathan-chay-709a8719a/>
+        <img src="../assets/linkedin.png" />
+      </a>
+
+      <a href=https://natetheneet.tumblr.com/>
+        <img src="../assets/tumblr.png" />
+      </a>
+    </div>
+
+    <div id="title-container">
+      <span class="title">natetheneet</span> <br> <br>
+      <span class="subtitle">software engineer</span>
+    </div>
   </div>
 </template>
 
@@ -12,10 +36,59 @@ export default {
 </script>
 
 <style scoped>
- #container {
-   padding: 50px;
-   background-color: rgb(30, 30, 30);
-   box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
-   border-radius: 10px;
- }
+#container {
+  display: flex;
+  flex-direction: row;
+  height: 70%;
+  padding: 30px;
+  background-color: rgb(30, 30, 30);
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
+  border-radius: 10px;
+}
+
+#title-container {
+  align-self: center;
+}
+
+#icons {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-right: 30px;
+}
+
+.title {
+  font-weight: bold;
+  font-size: 25pt;
+}
+
+.subtitle {
+  font-size: 14pt;
+}
+
+#icons a {
+  background-color: rgb(40, 40, 40);
+  border-radius: 5px;
+  padding: 5px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  width: 50px;
+  align-self: flex-end;
+  transition: width 200ms ease-in-out;
+}
+
+#icons img {
+  height: 40px;
+  width: 40px;
+  transition: filter 200ms ease-in-out;
+}
+
+#icons a:hover {
+  width: 100px;
+}
+
+#icons img:hover {
+  filter: blur(2px);
+}
 </style>
