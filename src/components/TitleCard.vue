29be46c4b34ace8b071c5256ae25fe2c42e4a108
nate <nathangchay@gmail.com>
2021-08-03 14:07:20 -0600

    Added animations with titles

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -3,22 +3,27 @@
     <div id="icons">
       <a href="https://github.com/natetheneet">
         <img src="../assets/github.png">
+        <span>github</span>
       </a>
 
       <a href="https://twitter.com/latencygirl">
         <img src="../assets/twitter.png">
+        <span>twitter</span>
       </a>
 
       <a href="https://www.instagram.com/natetheneet/">
         <img src="../assets/instagram.png">
+        <span>instagram</span>
       </a>
 
       <a href="https://www.linkedin.com/in/nathan-chay-709a8719a/">
         <img src="../assets/linkedin.png">
+        <span>linkedin</span>
       </a>
 
       <a href="https://natetheneet.tumblr.com/">
         <img src="../assets/tumblr.png">
+        <span>tumblr</span>
       </a>
     </div>
 
@@ -37,9 +42,9 @@ export default {
 
 <style scoped>
 #container {
+  position: relative;
   display: flex;
   flex-direction: row;
-  height: 70%;
   padding: 30px;
   background-color: rgb(30, 30, 30);
   box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
@@ -48,10 +53,12 @@ export default {
 
 #title-container {
   align-self: center;
+  flex: 1;
+  justify-self: flex-end;
 }
 
 #icons {
-  position: absolute;
+  width: 40px;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
@@ -68,14 +75,18 @@ export default {
 }
 
 #icons a {
-  background-color: rgb(40, 40, 40);
+  display: flex;
+  flex-direction: row-reverse;
+  background-color: rgb(50, 50, 50);
   border-radius: 5px;
   padding: 5px;
   margin-top: 5px;
   margin-bottom: 5px;
-  width: 50px;
+  width: 40px;
   align-self: flex-end;
+  justify-content: space-between;
   transition: width 200ms ease-in-out;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.2);
 }
 
 #icons img {
@@ -84,11 +95,25 @@ export default {
   transition: filter 200ms ease-in-out;
 }
 
-#icons a:hover {
-  width: 100px;
+#icons span {
+  color: white;
+  opacity: 0;
+  align-self: center;
+  margin-left: 10px;
+  margin-right: 15px;
+  text-decoration: none;
+  transition: opacity 200ms 100ms ease-in-out;
 }
 
-#icons img:hover {
+#icons a:hover {
+  width: 150px;
+}
+
+#icons a:hover img {
   filter: blur(2px);
+}
+
+#icons a:hover span {
+  opacity: 1;
 }
 </style>
