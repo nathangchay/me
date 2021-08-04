7d98ae77c1f493e4227a32d27eeb0444a5eb43ba
nate <nathangchay@gmail.com>
2021-08-04 15:43:13 -0600

    Started title component
    Unfinished - animation in progress

---
+++ src/components/Title.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p id="title">
+      {{ curText }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  data: () => ({
+    finalText: 'nate world',
+    curText: 'a',
+  }),
+  mounted() {
+    for (var i = 0; i < finalText.length; i++) {
+      setTimeout(() => {
+        this.curText += finalText.charAt(i);
+      }, 200)
+    }
+  },
+};
+
+
+</script>
+
+<style scoped>
+  #title {
+    font-size: 60pt;
+    font-weight: bold;
+  }
+</style>
