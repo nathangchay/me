45076a9e998e659c0cbceac302040d8afd918c10
nate <nathangchay@gmail.com>
2021-08-17 13:23:09 -0600

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <Title />
+  <TitleCard />
+</template>
+
+<script>
+import TitleCard from '../components/TitleCard.vue';
+import Title from '../components/Title.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    TitleCard,
+    Title,
+  },
+};
+</script>
+
+<style>
+
+</style>
