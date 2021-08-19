355db1f8d30cf0b46d2b7c0cfc743492e470c422
nate <nathangchay@gmail.com>
2021-08-19 16:00:47 -0600

    Update Projects.vue
    Add titles and featured project tile (wip)

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
-    hi
+    <h1>featured projects</h1>
+    <FeaturedProjectTile />
+    <h2>other projects</h2>
   </div>
 </template>
 
 <script>
+import FeaturedProjectTile from '../components/FeaturedProjectTile.vue';
+
 export default {
   name: 'Projects',
+  components: {
+    FeaturedProjectTile,
+  }
 };
 </script>
 
