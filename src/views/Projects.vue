4ee3bc307e2f81e305ea1bed93d2c4e58494bc1e
nate <nathangchay@gmail.com>
2021-08-31 15:50:21 -0600

    Pass props to tiles

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,22 +1,71 @@
 <template>
   <div>
     <h1>featured projects</h1>
-    <FeaturedProjectTile />
+    <div id="row">
+      <FeaturedProjectTile
+        title="project1"
+        subtitle="a sample project"
+        desc=" this is the description of the project. i made this because i was really bored. it's alright i guess. "
+        lang="vue, css"
+        date="aug. 2021"
+        link="https://github.com/natetheneet/arduino-spotify-controller"
+      />
+      <FeaturedProjectTile
+        title="project1"
+        subtitle="a sample project"
+        desc=" this is the description of the project. i made this because i was really bored. it's alright i guess. "
+        lang="vue, css"
+        date="aug. 2021"
+        link="https://github.com/natetheneet/arduino-spotify-controller"
+      />
+    </div>
     <h2>other projects</h2>
+    <div id="row">
+      <ProjectTile
+        title="project1"
+        subtitle="a sample project"
+        desc=" this is the description of the project. i made this because i was really bored. it's alright i guess. "
+        lang="vue, css"
+        date="aug. 2021"
+        link="https://github.com/natetheneet/arduino-spotify-controller"
+      />
+      <ProjectTile
+        title="project1"
+        subtitle="a sample project"
+        desc=" this is the description of the project. i made this because i was really bored. it's alright i guess. "
+        lang="vue, css"
+        date="aug. 2021"
+        link="https://github.com/natetheneet/arduino-spotify-controller"
+      />
+      <ProjectTile
+        title="project1"
+        subtitle="a sample project"
+        desc=" this is the description of the project. i made this because i was really bored. it's alright i guess. "
+        lang="vue, css"
+        date="aug. 2021"
+        link="https://github.com/natetheneet/arduino-spotify-controller"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import FeaturedProjectTile from '../components/FeaturedProjectTile.vue';
+import ProjectTile from '../components/ProjectTile.vue';
 
 export default {
   name: 'Projects',
   components: {
     FeaturedProjectTile,
+    ProjectTile,
   }
 };
 </script>
 
-<style>
-
+<style scoped>
+#row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
 </style>
