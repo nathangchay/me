36d38edc03d6735a01a36d3759fde6770497af5b
nate <nathangchay@gmail.com>
2021-09-01 15:04:07 -0600

    Create ContactButton.vue

---
+++ src/components/ContactButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <a
+    id="button"
+    :href="link"
+  >
+    {{ text }}
+  </a>  
+</template>
+
+<script>
+export default {
+  name: "ContactButton",
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+    link: {
+      type: String,
+      default: "",
+    },
+  }
+}
+</script>
+
+<style scoped>
+  #button {
+    text-decoration: none;
+    color: var(--text);
+    background-color: var(--container);
+    padding: 15px;
+    border-radius: 15px;
+    border: solid transparent 2px;
+    cursor: pointer;
+    box-shadow: 0 3px 1px rgb(0 0 0 / 0.2);
+    transition: border-color 200ms ease-in-out, background-color 300ms ease-in-out;
+  }
+
+  #button:hover {
+  border-color: var(--text)
+}
+</style>
