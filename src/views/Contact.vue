7e981430d3887d39d4f0de6735644406f23fda50
nate <nathangchay@gmail.com>
2021-09-01 15:04:18 -0600

    Add contact buttons

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    contact
+    you can contact me via:
+    <ContactButton
+      text="e-mail"
+      link="mailto:nathangchay@gmail.com"
+    />
+    <ContactButton
+      text="e-mail"
+      link="mailto:nathangchay@gmail.com"
+    />
   </div>
 </template>
 
 <script>
+import ContactButton from '../components/ContactButton.vue';
+
 export default {
   name: 'Contact',
+  components: {
+    ContactButton,
+  }
 };
 </script>
 
