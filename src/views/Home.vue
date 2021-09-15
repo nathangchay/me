b8b2a5a9fb4086cc62ea0447889c61ff40af2c2b
nate <nathangchay@gmail.com>
2021-09-15 15:02:24 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <Title />
+
+  <h1>
+    about me
+  </h1>
+  <TitleCard />
 </template>
 
 <script>
 import Title from '../components/Title.vue';
+import TitleCard from '../components/TitleCard.vue'
 
 export default {
   name: 'Home',
   components: {
     Title,
+    TitleCard,
   },
 };
 </script>
 
-<style>
-
+<style scoped>
 </style>
