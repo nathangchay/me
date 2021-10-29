6e5db3021e8f8025da60200b7c50e7d0ec579f76
nate <nathangchay@gmail.com>
2021-10-29 14:17:25 -0600

    Added maze page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue';
 import Projects from '../views/Projects.vue';
 import Contact from '../views/Contact.vue';
 import Mandelbrot from '../views/Mandelbrot.vue';
+import Maze from '../views/Maze.vue';
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: '/mandelbrot',
     name: 'Mandelbrot',
     component: Mandelbrot,
+  },
+  {
+    path: '/maze',
+    name: 'Maze',
+    component: Maze,
   },
 ]
 
---
+++ src/views/Maze.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>
+    maze generator
+  </h1>
+
+  <div id="container">
+    <div id="tile" />
+  </div>
+</template>
+
+<style scoped>
+#container {
+  display: grid;
+}
+</style>
